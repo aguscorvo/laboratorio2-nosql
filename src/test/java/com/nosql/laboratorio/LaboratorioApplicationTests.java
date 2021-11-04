d9b710cd46f09cc1fee66be289915de7e6e3d6fe
aguscorvo <aguscorvo@gmail.com>
2021-11-04 17:18:47 -0300

    Initial commit

---
+++ src/test/java/com/nosql/laboratorio/LaboratorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nosql.laboratorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
